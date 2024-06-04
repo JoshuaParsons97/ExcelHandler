097c5143ad4d5a8baa1d680f1d9ddee6f74e5673
JoshuaParsons97 <127176036+JoshuaParsons97@users.noreply.github.com>
2024-06-04 22:52:01 +0200

    Initial commit
    Untested read code
    Control flow for test interface

---
+++ ExcelHandler/Read.cs
@@ -0,0 +1,81 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Data;
+
+namespace ExcelHandler
+{
+    public class Read
+    {
+        public static DataSet ReadToDataSet(string FilePath, bool ColumnNamesSpecified = true)
+        {
+            if (File.Exists(FilePath))
+            {
+                try
+                {
+                    DataSet DataSet = new DataSet();
+
+                    using var Document = SpreadsheetDocument.Open(FilePath, false);
+
+                    var Workbook = Document.WorkbookPart;
+
+                    if (Workbook != null)
+                    {
+                        var workbook = Workbook.Workbook;
+
+                        var Sheets = workbook.Descendants<Sheet>();
+                        foreach (var sheet in Sheets)
+                        {
+                            if (sheet.Id != null)
+                            {
+                                var Worksheet = (WorksheetPart)Workbook.GetPartById(sheet.Id);
+
+                                DataTable DataTable = new();
+
+                                Row[] Rows = Worksheet.Worksheet.Descendants<Row>().ToArray();
+
+                                for (int RowCount = Rows.Count(); RowCount > 0; RowCount--)
+                                {
+                                    Row CurrentRow = Rows[RowCount];
+                                    Cell[] Cells = CurrentRow.Elements<Cell>().ToArray();
+
+                                    if (ColumnNamesSpecified && RowCount <= 0)
+                                    {
+                                        //The first row contains column names, lets add the inner text as column names
+                                        for (int CellCount = Rows.Count(); CellCount > 0; CellCount--)
+                                        {
+                                            Cell CurrentCell = Cells[CellCount];
+                                            DataTable.Columns.Add(CurrentCell.CellValue != null ? CurrentCell.CellValue.InnerText : "");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        //Populate a new row with data and push it to the sheet
+                                        DataRow NewRow = DataTable.NewRow();
+                                        for (int CellCount = Rows.Count(); CellCount > 0; CellCount--)
+                                        {
+                                            Cell CurrentCell = Cells[CellCount];
+                                            NewRow[CellCount] = CurrentCell.CellValue != null ? CurrentCell.CellValue.InnerText : "";
+                                        }
+                                    }
+                                }
+
+                                DataSet.Tables.Add(DataTable);
+                            }
+                        }
+                    }
+
+                    return DataSet;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException("The file could not be found, please check that the file path is correct");
+            }
+
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,52 @@
+﻿public partial class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please select an operation to run, enter the number from he list:");
+        Console.WriteLine("1. Excelhandler");
+
+        ConsoleKeyInfo Selection = Console.ReadKey();
+
+        Console.WriteLine("Please confirm that you want to execute the selected operation (" + Selection.Key + ") y/n");
+
+        ConsoleKeyInfo ConfirmationKey = Console.ReadKey();
+        if (ConfirmationKey.Key == ConsoleKey.Y)
+        {
+            Console.WriteLine("Operation confirmed... Starting Process");
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Operation cancelled... Lets try again");
+            Main(args);
+        }
+
+        try
+        {
+            switch (Selection.Key)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    Console.WriteLine("Please enter the fully qualified file path of the excel sheet you want to use as a test subject");
+                    var FilePath = Console.ReadLine();
+                    if (FilePath != null)
+                    {
+                        ExcelHandler.Read.ReadToDataSet(FilePath);
+                    }
+
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("The selection made does not exist, please choose an operation number from the list provided");
+                    Main(args);
+                    break;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Main(args);
+        }
+    }
+}
+
